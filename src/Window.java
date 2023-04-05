5d62b2da248c0c703568bbeea156dc1796c13886
Isaac Pascoe <IsaacMPascoe@gmail.com>
2023-04-05 09:45:50 +0800

    Create Window.java

---
+++ src/Window.java
@@ -0,0 +1,28 @@
+
+import java.awt.*;
+import javax.swing.*;
+
+public class Window{
+
+    public Window() {
+
+        JFrame frame = new JFrame();
+        frame.setTitle("Title of Window");// JFrame ("The title of the window goes in here")
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JLabel textLabel = new JLabel("Window Label!", SwingConstants.CENTER);
+        textLabel.setPreferredSize(new Dimension(300, 100));
+        frame.getContentPane().add(textLabel, BorderLayout.CENTER);
+
+        frame.setLocationRelativeTo(null);
+        frame.pack();
+        frame.setVisible(true);
+
+    }
+
+    public static void main(String[]args) {
+
+        Window window = new Window();
+
+    }
+}
